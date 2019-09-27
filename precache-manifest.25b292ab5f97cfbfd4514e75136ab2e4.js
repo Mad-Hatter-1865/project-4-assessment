815e654f05dc931d16f5cd52a9a2bd6af056b736
Michael Martinez <rusalka2100@gmail.com>
2019-09-27 18:16:31 -0500

    Updates

---
+++ precache-manifest.25b292ab5f97cfbfd4514e75136ab2e4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "059eb830d23211eeba216dab01954f8c",
+    "url": "/project-4-assessment/index.html"
+  },
+  {
+    "revision": "94240917b962c933a4f0",
+    "url": "/project-4-assessment/static/css/main.bda7fd81.chunk.css"
+  },
+  {
+    "revision": "5fea86612fcf66d20df0",
+    "url": "/project-4-assessment/static/js/2.a24a6d0d.chunk.js"
+  },
+  {
+    "revision": "94240917b962c933a4f0",
+    "url": "/project-4-assessment/static/js/main.97538994.chunk.js"
+  },
+  {
+    "revision": "8558f1f2a39bbac7d4e8",
+    "url": "/project-4-assessment/static/js/runtime-main.817893b6.js"
+  }
+]);
